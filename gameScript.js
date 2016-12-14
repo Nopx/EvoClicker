d80fcefeee4c24cd0aefe80a51c60cf5adbd660b
nopx <jollans94@googlemail.com>
2016-12-15 00:13:29 +0100

    Added general Layout and style

--- gameScript.js
+++ gameScript.js
@@ -5,7 +5,7 @@
   var structureCookie = "structure";
   var clickCookie = "clicks";
   var clicks = 0;
-
+  var structures = {};
 
 function start(){
   clicks = parseInt(getCookie(clickCookie));
@@ -13,12 +13,12 @@ function start(){
   if(isNaN(clicks)) clicks = 0;
   $(clickCounter).html(clicks);
 
-  stage = new createjs.Stage("demoCanvas");
+  stage = new createjs.Stage("mainCanvas");
   var circle = new createjs.Shape();
   circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
   circle.addEventListener("click", function(event) {clickFunction();});
-  circle.x = 100;
-  circle.y = 100;
+  circle.x = stage.canvas.width/2;
+  circle.y = stage.canvas.height/2;
   stage.addChild(circle);
   stage.update();
 }
@@ -33,6 +33,20 @@ function saveGlobalState(){
   saveCookie(clickCookie,clicks);
   saveCookie(structureCookie,"XXXXXXXXXX");
   saveCookie(timeCookie,"TIME");
+}
+
+function initializeStructures(){
+  var structureList = getCookie(structureCookie).split(",");
+  for(var i =0; i< structureList.length; i++){
+    var structure = structureList[i].split(":");
+    var structureName = structure[0];
+    var structureAmount = structure[1];
+    buildStructure(structureName,structureAmount);
+  }
+}
+
+function buildStructure(structureName,structureAmount){
+  structures[structureName] = structureAmount;
 }
 
 function getCookie(key){
